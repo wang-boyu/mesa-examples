d258a8bbb11627705fa856b54a9504c2234c420e
Wang Boyu <boyu.wby@gmail.com>
2023-11-03 11:19:24 -0400

    wip: solora frontend for geospace

---
+++ gis/geo_schelling_points/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="geo_schelling_points",
+    packages=find_packages(),
+    version="0.1.0",
+    description="GeoSchelling Model (Points & Polygons)",
+    author="Wang Boyu",
+    license="",
+)
